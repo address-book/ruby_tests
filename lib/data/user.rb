1a010e4b502424bcdbbeef79c087446c71c87f0e
Titus Fortner <titusfortner@gmail.com>
2019-03-04 17:04:00 -0600

    random data

--- lib/data/user.rb
+++ lib/data/user.rb
@@ -1,6 +1,14 @@
+require 'faker'
+
 module RubyTests
   class User
     attr_accessor :email, :password
+
+    def self.random
+      email = Faker::Internet.email
+      password = Faker::Internet.password
+      User.new(email: email, password: password)
+    end
 
     def initialize(opt)
       @email = opt[:email]
---
+++ spec/authentication_tests/sign_up_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe "Authentication" do
+  let(:driver) { @driver }
+  let(:driver) { @driver }
+  let(:wait) { Selenium::WebDriver::Wait.new(timeout: 10) }
+  let(:select) { Selenium::WebDriver::Support::Select }
+
+  it 'signs up user' do
+    driver.navigate.to 'http://a.testaddressbook.com/sign_up'
+
+    user = User.random
+
+    driver.find_element(id: 'user_email').send_keys user.email
+    driver.find_element(id: 'user_password').send_keys user.password
+    driver.find_element(css: "input[data-test='submit']").click
+  end
+end
