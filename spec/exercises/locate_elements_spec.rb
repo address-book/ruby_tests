71c8fbe56b6189eda714425f4bd38217661b532d
Titus Fortner <titusfortner@gmail.com>
2019-03-04 00:34:48 -0600

    EXERCISE - basic location

---
+++ spec/exercises/locate_elements_spec.rb
@@ -0,0 +1,70 @@
+RSpec.describe 'Element Location Exercise' do
+  before { @driver.get "http://a.testaddressbook.com/sign_in" }
+
+  it 'locates email field' do
+
+    id = ""
+    name = ""
+    class_name = ""
+    tag_name = ""
+    tag_index = 0
+
+
+    # Obfuscated way of getting the Email Field
+    email_field = @driver.find_elements(:xpath, "//*")[30]
+
+    # Assert correct element has been found withe the updated strings
+    if email_field != @driver.find_element(:id, id)
+      raise "#{id} is not the correct id for the email field"
+    elsif email_field != @driver.find_element(:name, name)
+      raise "#{name} is not the correct name for the email field"
+    elsif email_field != @driver.find_element(:class_name, class_name)
+      raise "#{class_name} is not the correct class name for the email field"
+    elsif email_field != @driver.find_elements(:tag_name, tag_name)[tag_index]
+      raise "#{tag_name} with index #{tag_index} is not the correct combination for the email field"
+    end
+  end
+
+  it 'locates sign in button' do
+
+    name = ""
+    class_name = ""
+    tag_name = ""
+    tag_index = 0
+
+
+    # Obfuscated way of getting the Email Field
+    sign_in_button = @driver.find_elements(:xpath, "//*")[34]
+
+    # Assert correct element has been found withe the updated strings
+    if sign_in_button != @driver.find_element(:name, name)
+      raise "#{name} is not the correct name for the sign in button"
+    elsif sign_in_button != @driver.find_element(:class_name, class_name)
+      raise "#{class_name} is not the correct Class Name for the sign in button"
+    elsif sign_in_button != @driver.find_elements(:tag_name, tag_name)[tag_index]
+      raise "#{tag_name} with index #{tag_index} is not the correct combination for the sign in button"
+    end
+  end
+
+  it 'locates sign up link' do
+
+    link_text = ""
+    partial_link_text = ""
+    tag_name = ""
+    tag_index = 0
+
+
+    # Obfuscated way of getting the Email Field
+    email_field = @driver.find_elements(:xpath, "//*")[36]
+
+    # Assert correct element has been found withe the updated strings
+    if email_field != @driver.find_element(:link_text, link_text)
+      raise "#{link_text} is not the correct Link Text for the sign up link"
+    elsif email_field != @driver.find_element(:partial_link_text, partial_link_text)
+      raise "#{partial_link_text} is not the correct Partial Link Text for the sign up link"
+    elsif email_field != @driver.find_elements(:tag_name, tag_name)[tag_index]
+      raise "#{tag_name} with index #{tag_index} is not the correct combination for the sign up link"
+    end
+  end
+
+end
