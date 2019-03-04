13d8ef6d347a9c26764203667d6d0b5aed207e56
Titus Fortner <titusfortner@gmail.com>
2019-03-04 17:39:34 -0600

    implement page object elements

--- lib/pages/sign_in.rb
+++ lib/pages/sign_in.rb
@@ -2,6 +2,11 @@ module RubyTests
   class SignIn
     attr_reader :driver
 
+    SESSION_EMAIL = {id: 'session_email'}
+    SESSION_PASSWORD = {id: 'session_password'}
+    SUBMIT_BUTTON = {css: "input[data-test='submit']"}
+    SUCCESS = {css: "span[data-test='current-user']"}
+
     def self.visit(driver)
       driver.get "http://a.testaddressbook.com/sign_in"
       new(driver)
@@ -12,13 +17,15 @@ module RubyTests
     end
 
     def sign_in(user)
-      driver.find_element(id: 'session_email').send_keys user.email
-      driver.find_element(id: 'session_password').send_keys user.password
-      driver.find_element(css: "input[data-test='submit']").click
+      @driver.find_element(SESSION_EMAIL).send_keys user.email
+      @driver.find_element(SESSION_PASSWORD).send_keys user.password
+      @driver.find_element(SUBMIT_BUTTON).click
     end
 
     def success?
-      driver.find_elements(css: "span[data-test='current-user']").size > 0
+      driver.find_elements(SUCCESS).size > 0
     end
+
   end
 end
+
