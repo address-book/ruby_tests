de52c6bd3ddb5bbb5a51ed79910015fb56e39727
Titus Fortner <titusfortner@gmail.com>
2019-03-05 07:59:45 -0600

    EXERCISE: a Test Journey

---
+++ spec/exercises/journey_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe 'Test Journey' do
+  it 'creates reads updates and deletes address' do
+    # Navigate
+
+    # Click Sign In Link
+
+    # Sign In (see `spec/exercises/element_actions_spec.rb`)
+
+    # Click Addresses Link
+
+    # Click Create Address Link
+
+    # Create Address
+
+    # Validate Address Information
+
+    # Click Edit
+
+    # Change Address Information
+
+    # Validate Updated Address Information
+
+    # Click List
+
+    # Destroy the Address You Created
+
+  end
+end
