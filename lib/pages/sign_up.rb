20360796b0d80c5a2338ace7ffb4a13b017ea149
Titus Fortner <titusfortner@gmail.com>
2019-03-05 07:42:51 -0600

    solution to PO implementation exercise

--- lib/pages/sign_up.rb
+++ lib/pages/sign_up.rb
@@ -3,9 +3,15 @@ module RubyTests
     attr_reader :driver
 
     # Define elements as class constants
+    USER_EMAIL = {id: 'user_email'}
+    USER_PASSWORD = {id: 'user_password'}
+    SUBMIT_BUTTON = {css: "input[data-test='submit']"}
+    SUCCESS = {css: "span[data-test='current-user']"}
 
     # Define visit
     def self.visit(driver)
+      driver.get "http://a.testaddressbook.com/sign_up"
+      new(driver)
     end
 
     def initialize(driver)
@@ -13,6 +19,15 @@ module RubyTests
     end
 
     # Define necessary methods
+    def sign_up(user = User.new)
+      @driver.find_element(USER_EMAIL).send_keys user.email
+      @driver.find_element(USER_PASSWORD).send_keys user.password
+      @driver.find_element(SUBMIT_BUTTON).click
+    end
+
+    def success?
+      driver.find_elements(SUCCESS).size > 0
+    end
   end
 end
 
--- spec/authentication_tests/sign_up_spec.rb
+++ spec/authentication_tests/sign_up_spec.rb
@@ -2,9 +2,9 @@ RSpec.describe "Authentication" do
   let(:driver) { @driver }
 
   it 'signs up user' do
-    sign_in_page = SignUp.visit(@driver)
+    sign_up_page = SignUp.visit(@driver)
 
-    sign_in_page.sign_up
+    sign_up_page.sign_up
 
     expect(sign_up_page.success?).to eq true
   end
